77245358726271b20a2f57308ba036e1139e846a
Oleksandr Avoyants <shhavel@gmail.com>
2017-04-18 03:55:01 +0300

    Test case with invalid algorithm option. Test usage instructions :construction:

--- spec/features/jwtgen_spec.rb
+++ spec/features/jwtgen_spec.rb
@@ -3,14 +3,28 @@ require "clipboard"
 
 RSpec.describe "CLI", type: :aruba do
   context "with invalid options" do
-    before do
-      run "jwtgen --key my$ecretK3y --algorithm HS512 "\
-          "--payload user_id=1123,email=INVALID,first_name=Jack,last_name=Hackett"
+    context "invalid email" do
+      before do
+        run "jwtgen --key my$ecretK3y --algorithm HS512 "\
+            "--payload user_id=1123,email=INVALID,first_name=Jack,last_name=Hackett"
+      end
+
+      it "exits with status 1 and prints error message" do
+        expect(last_command_started).to have_exit_status(1)
+        expect(last_command_started).to have_output "** Error occurred: Email is invalid. Get help: jwtgen --help"
+      end
     end
 
-    it "exits with status 1 and prints error message" do
-      expect(last_command_started).to have_exit_status(1)
-      expect(last_command_started).to have_output "** Error occurred: Email is invalid. Get help: jwtgen --help"
+    context "unknown algorithm" do
+      before do
+        run "jwtgen --key my$ecretK3y --algorithm UNKNOWN "\
+            "--payload user_id=1123,email=jack.hackett@example.com,first_name=Jack,last_name=Hackett"
+      end
+
+      it "exits with status 1 and prints error message" do
+        expect(last_command_started).to have_exit_status(1)
+        expect(last_command_started).to have_output include_output_string "invalid"
+      end
     end
   end
 
@@ -29,6 +43,24 @@ RSpec.describe "CLI", type: :aruba do
         "email"=>"jack.hackett@example.com",
         "first_name"=>"Jack",
         "last_name"=>"Hackett"})
+    end
+  end
+
+  context "usage instructions" do
+    before do
+      run "jwtgen --help"
+    end
+
+    it "generates JWT and copies it into clipboard" do
+      expect(last_command_started).to be_successfully_executed
+      expect(last_command_started).to have_output <<-EOF.gsub(/^ {8}/, '').chomp
+        Usage: jwtgen [options]
+                --payload key1=value1,key2=value2,key3=value3
+                                             JWT payload. Required keys: `user_id` and `email`.
+                --separator SEPARATOR        Payload key-value separator (default "=")
+                --key KEY                    HMAC secret key
+                --algorithm ALGORITHM        Cryptographic hash algorithm, one of: none, HS256 HS512256 HS384 HS512
+      EOF
     end
   end
 end
